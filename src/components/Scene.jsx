e6ac87920a878158d9410d6edac58457a47ecd61
Tonny <84282878+UzCoderOff@users.noreply.github.com>
2024-09-05 01:05:17 +0500

    not finished

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,14 +1,18 @@
-import { Canvas } from '@react-three/fiber'
-import React from 'react'
+import { CameraControls, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import React from "react";
+import Robber from './Robber'
 
 const Scene = () => {
   return (
     <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        
+      <color attach="background" args={["#111"]} />
+      <CameraControls zoom={true}/>
+      <ambientLight intensity={1}/>
+      <pointLight intensity={10} position={[10, 10, 10]} />
+      <Robber/>
     </Canvas>
-  )
-}
+  );
+};
 
-export default Scene
+export default Scene;
